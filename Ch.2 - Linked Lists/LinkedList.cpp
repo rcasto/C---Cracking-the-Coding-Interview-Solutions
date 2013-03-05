31ea46e3ec59eea58bdb64a365098622c1d4fb2b
Richard Casto <rcasto92@gmail.com>
2013-03-05 17:59:33 -0500

    Just did LinkedList Problem 1

--- Ch.2 - Linked Lists/LinkedList.cpp
+++ Ch.2 - Linked Lists/LinkedList.cpp
@@ -4,10 +4,11 @@
 template <class T>
 class LinkedListNode {
 
+public:
+
 	LinkedListNode *next;
 	T data;
 
-public:
 	LinkedListNode(T data) {
 		this->data = data;
 		this->next = 0;
@@ -31,14 +32,6 @@ public:
 		}
 	}
 
-	T getData() {
-		return this->data;
-	}
-
-	LinkedListNode *getNext() {
-		return this->next;
-	}
-
 };
 
 /* Cracking the Coding Interview Problems */
@@ -46,18 +39,53 @@ public:
 /* 
 	Problem 1
 	Implement a method to remove duplicates from an unsorted linked list
-	How would you solve this problem if a temporary buffer were not allowed? Use two pointers O(n^2)
+	How would you solve this problem if a temporary buffer were not allowed? Use two pointers O(n^2) time, O(1) space
+
+	The method directly below has O(n) time and O(n) space complexity
 */
-void removeDups(LinkedListNode<int> list) {
+void removeDups(LinkedListNode<int> *list) {
+	if (!list) {
+		return;
+	}
+	LinkedListNode<int> *prev = list, *next = list->next;
 	std::set<int> nums;
 	std::pair<std::set<int>::iterator, bool> ret;
-	nums.insert(list.getData());
-	LinkedListNode *next = list.getNext();
+	//Make initial insert
+	nums.insert(list->data);
+	//Check next data to make sure not already in list
 	while (next) {
-		ret = nums.insert(next->getData());
-		next = next->getNext();
-		if (next && !ret.second) {
-			next = next->getNext();
+		ret = nums.insert(next->data);
+		if (!ret.second) {
+			prev->next = next->next;
+			delete next;
+			next = prev->next;
+		} else {
+			prev = next;
+			next = next->next;
+		}
+	}
+}
+
+/* 
+	Implementation of remove dups that does not require any extra space or temporary buffer
+*/
+void removeDups2(LinkedListNode<int> *list) {
+	LinkedListNode<int> *current = list, *prev = list, *next = list->next;
+	while (current) {
+		while (next) {
+			if (next->data == current->data) {
+				prev->next = next->next;
+				delete next;
+				next = prev->next;
+			} else {
+				prev = next;
+				next = next->next;
+			}
+		}
+		current = current->next;
+		prev = current;
+		if (current) {
+			next = current->next;
 		}
 	}
 }
@@ -65,12 +93,17 @@ void removeDups(LinkedListNode<int> list) {
 
 /* Debugging/Testing */
 int main(int argc, char *argv[]) {
-	LinkedListNode<int> list(3);
-	list.add(5);
-	list.add(2);
-	list.add(10);
-	list.printList();
-	removeDups(list);
-	list.printList();
+	LinkedListNode<int> *list = new LinkedListNode<int>(3);
+	list->add(5);
+	list->add(2);
+	list->add(10);
+	list->add(5);
+	list->add(2);
+	list->add(3);
+	list->add(5);
+	list->add(9);
+	list->printList();
+	removeDups2(list);
+	list->printList();
 	return 0;
 }
