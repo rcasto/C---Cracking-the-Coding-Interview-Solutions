887d558f713e2dcc626a80bac4e4ba85706505a6
Richard Casto <rcasto92@gmail.com>
2013-03-06 20:44:20 -0500

    Just did LinkedList Problems 2 and 3

--- Ch.2 - Linked Lists/LinkedList.cpp
+++ Ch.2 - Linked Lists/LinkedList.cpp
@@ -37,7 +37,7 @@ public:
 /* Cracking the Coding Interview Problems */
 
 /* 
-	Problem 1
+	Problem 1:
 	Implement a method to remove duplicates from an unsorted linked list
 	How would you solve this problem if a temporary buffer were not allowed? Use two pointers O(n^2) time, O(1) space
 
@@ -90,6 +90,63 @@ void removeDups2(LinkedListNode<int> *list) {
 	}
 }
 
+/*
+	Problem 2:
+	Implement a method to find the kth to last element in the linked list
+*/
+LinkedListNode<int> *kthToLast(LinkedListNode<int> *list, int k) {
+	if (!list) {
+		return 0;
+	}
+	LinkedListNode<int> *kth = list, *node = list;
+	while (k > 0 && node) {
+		node = node->next;
+		k--;
+	}
+	if (!node) {
+		return 0;
+	}
+	while (node->next) {
+		kth = kth->next;
+		node = node->next;
+	}
+	return kth;
+}
+
+/*
+	Problem 3:
+	Implement a method to delete a node from the middle of a Linked List given only access to that
+	node
+
+	What I have done below is not entirely necessary, you only need to switch the current node with the next node
+	you do not need to traverse the entire linked list copying over data from next node
+
+	Algorithm:
+	-copy data from next node to current node
+	-save next next node
+	-delete next node
+	-adjust next node to point to next next node that was saved
+*/
+void deleteNode(LinkedListNode<int> *node) {
+	if (!node) {
+		return;
+	}
+	LinkedListNode<int> *prev = 0;
+	while (node->next) {
+		node->data = node->next->data;
+		prev = node;
+		node = node->next;
+	}
+	delete node;
+	if (prev) {
+		prev->next = 0;
+	}
+}
+
+/*
+	Problem 4:
+*/
+
 
 /* Debugging/Testing */
 int main(int argc, char *argv[]) {
@@ -104,6 +161,9 @@ int main(int argc, char *argv[]) {
 	list->add(9);
 	list->printList();
 	removeDups2(list);
+	list->printList();
+	LinkedListNode<int> *kth = kthToLast(list, 3);
+	deleteNode(kth);
 	list->printList();
 	return 0;
 }
